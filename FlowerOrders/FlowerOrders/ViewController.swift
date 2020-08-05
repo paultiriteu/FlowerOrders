54c9bff541700ad149a84387688d23fae116b5b7
Paul Tiriteu <paul.tiriteu@greppysystems.com>
2020-08-05 17:49:22 +0300

    project init

---
+++ FlowerOrders/FlowerOrders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlowerOrders
+//
+//  Created by Paul Tiriteu on 05/08/2020.
+//  Copyright © 2020 Paul Tiriteu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
